42f4a2e1d6572ee55c54c9e1ee679d8f094b7ea1
Piotr Czerski <blater@poczta.fm>
2017-07-02 16:55:52 +0200

    Jeszcze do poprawki

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+$(document).ready(function () {
+    $(window).scroll();
+});
+
+// Wyrzucamy definicję funkcji na zewnątrz jQuery
+$(window).scroll(function () {
+    changeMenu();
+});
+
+function changeMenu() {
+    var navHeight = $('.navbar').height();
+    var currentPosition = $(window).scrollTop();
+    console.log(navHeight);
+    console.log(currentPosition);
+    if ($(window).scrollTop() > navHeight) {
+        $('.navbar').addClass('klasa');
+        $('#main-header nav').css({background-color: '#ffffff'});
+        $('.navbar').css({transition: 'all .5s'});
+    } else {
+        $('.navbar').removeClass('klasa');
+    }
+};
